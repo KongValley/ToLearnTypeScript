1aaedbaac43aec00a43812042e55aba452f1b5c6
KongValley <kzp13968320676@163.com>
2019-12-29 00:34:55 +0800

    :tada: Init repo
    add basic types

---
+++ types_array.ts
@@ -0,0 +1,9 @@
+/* 数组类型
+-------------------------- */
+const numberArr: number[] = [2333]
+const stringArr: string[] = ['Apple', 'Banana']
+/* 使用数组泛型
+-------------------------- */
+const otherNameArr: Array<number> = [1, 2, 3]
+
+const doubleTypeArr: (string | number)[] = [123, 'Apple']
---
+++ types_tuple.ts
@@ -0,0 +1,3 @@
+/* 元组类型
+-------------------------- */
+const tuple: [string, number, boolean] = ['a', 1, true]
