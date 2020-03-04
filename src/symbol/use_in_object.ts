f34bb38675cd505cce5fb8853831e8ca44b46779
Chara <chara1997@foxmail.com>
2020-03-04 10:18:45 +0800

    Update: 更新语法规范

--- src/symbol/use_in_object.ts
+++ src/symbol/use_in_object.ts
@@ -6,7 +6,7 @@ const info = {
   [prop]: 'chara'
 };
 
-console.log('%c 🍏 info: ', 'font-size:20px;background-color: #ED9EC7;color:#fff;', info);
+console.log('info: ', info);
 
 const s4 = Symbol('name');
 
@@ -14,12 +14,12 @@ const info_2 = {
   [s4]: 'chara',
   age: 18,
   sex: 'man'
-}
+};
 
-console.log('%c 🍉 info_2: ', 'font-size:20px;background-color: #FFDD4D;color:#fff;', info_2);
+console.log('info_2: ', info_2);
 
-info_2[s4] = '2333'
-console.log('%c 🍪 info_2: ', 'font-size:20px;background-color: #4b4b4b;color:#fff;', info_2);
+info_2[s4] = '2333';
+console.log('info_2: ', info_2);
 
 /* ①
 -------------------------- */
@@ -29,33 +29,33 @@ for (const key in info_2) {
 
 /* ②
 -------------------------- */
-console.log('%c 🥃 Object.keys(info_2): ', 'font-size:20px;background-color: #EA7E5C;color:#fff;', Object.keys(info_2));
+console.log('Object.keys(info_2): ', Object.keys(info_2));
 // Output: ["age", "sex"]
 
 /* ③
 -------------------------- */
-console.log('%c 🍊 Object.getOwnPropertyNames(info_2): ', 'font-size:20px;background-color: #E41A6A;color:#fff;', Object.getOwnPropertyNames(info_2));
+console.log('Object.getOwnPropertyNames(info_2): ', Object.getOwnPropertyNames(info_2));
 // Output: ["age", "sex"]
 
 /* ④
 -------------------------- */
-console.log('%c 🍦 JSON.stringify(info_2): ', 'font-size:20px;background-color: #42b983;color:#fff;', JSON.stringify(info_2));
+console.log('JSON.stringify(info_2): ', JSON.stringify(info_2));
 // Output: {"age":18,"sex":"man"}
 
 /* 以上四种无法获取到Symbol作为属性名的值
 -------------------------- */
 
-console.log('%c 🍝 Object.getOwnPropertySymbols(info_2): ', 'font-size:20px;background-color: #33A5FF;color:#fff;', Object.getOwnPropertySymbols(info_2));
+console.log('Object.getOwnPropertySymbols(info_2): ', Object.getOwnPropertySymbols(info_2));
 // Output: [Symbol(name)]
 
-console.log('%c 🌮 Reflect.ownKeys(info_2): ', 'font-size:20px;background-color: #465975;color:#fff;', Reflect.ownKeys(info_2));
+console.log('Reflect.ownKeys(info_2): ', Reflect.ownKeys(info_2));
 // Output: ["age", "sex", Symbol(name)]
 
 // Symbol的静态方法
-const s5 = Symbol('chara')
-const s6 = Symbol('chara')
+const s5 = Symbol(`chara`);
+const s6 = Symbol('chara');
 
-// console.log('%c 🍨 s5 === s6: ', 'font-size:20px;background-color: #42b983;color:#fff;', s5 === s6);
+// console.log('s5 === s6: ', s5 === s6);
 // Output: false
 
 /* 和 Symbol() 不同的是
@@ -64,15 +64,15 @@ const s6 = Symbol('chara')
  * 它会首先检查给定的 key 是否已经在注册表中了.
  * 假如是，则会直接返回上次存储的那个.否则，它会再新建一个.
 -------------------------- */
-const s7 = Symbol.for('chara')
-const s8 = Symbol.for('chara')
-const s9 = Symbol.for('23333')
-// console.log('%c 🍞 s7 === s8: ', 'font-size:20px;background-color: #4b4b4b;color:#fff;', s7 === s8);
+const s7 = Symbol.for('chara');
+const s8 = Symbol.for('chara');
+const s9 = Symbol.for('23333');
+// console.log('s7 === s8: ', s7 === s8);
 // Output: true
 
-console.log('%c 🥠 Symbol.keyFor(s7): ', 'font-size:20px;background-color: #4b4b4b;color:#fff;', Symbol.keyFor(s7));
+console.log('Symbol.keyFor(s7): ', Symbol.keyFor(s7));
 // Output: Chara
 
-console.log('%c 🍺 Symbol.keyFor(s5): ', 'font-size:20px;background-color: #ED9EC7;color:#fff;', Symbol.keyFor(s5));
+console.log('Symbol.keyFor(s5): ', Symbol.keyFor(s5));
 // Output: Undefined
 
